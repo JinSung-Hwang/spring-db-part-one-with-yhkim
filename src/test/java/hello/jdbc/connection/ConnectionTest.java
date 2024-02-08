b808a8b2393faf4134403b69b443574ff77a3395
JinSung-Hwang <wlstjd7782@gmail.com>
2024-02-08 23:57:24 +0900

    feature: 섹션2 - 커넥션풀과 데이터소스 이해 - DataSource예제1 DriverManager

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,40 @@
+package hello.jdbc.connection;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Slf4j
+public class ConnectionTest {
+
+  @Test
+  void driverManager() throws SQLException {
+    Connection con1 = DriverManager.getConnection(URL, USER, PASSWORD);
+    Connection con2 = DriverManager.getConnection(URL, USER, PASSWORD);
+    log.info("connection={}, class={}", con1, con1.getClass());
+    log.info("connection={}, class={}", con2, con2.getClass());
+  }
+
+  @Test
+  void dataSourceDriverManager() throws SQLException {
+    // note: DriverManagerDataSource는 커넥션 생성과 커넥션을 가져오는 부분이 분리되어있다.
+    // note: 이런 방식의 장점은 getConnection을 호출할때마다 설정 정보를 넣어주지 않아도 된다.
+    // note: 예를 들어 repository가 10개이면 repository 마다 connection 정보를 넣지 않아도 된다.
+    DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USER, PASSWORD);
+    useDataSource(dataSource);
+  }
+
+  private void useDataSource(DataSource dataSource) throws SQLException {
+    Connection con1 = dataSource.getConnection();
+    Connection con2 = dataSource.getConnection();
+    log.info("connection={}, class={}", con1, con1.getClass());
+    log.info("connection={}, class={}", con2, con2.getClass());
+  }
+
+}
